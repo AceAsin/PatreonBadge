1a2736a291809e520df25345b0485cc51530868c
Ace Asin <aceasin@gmail.com>
2020-08-23 13:48:29 -0600

    Ace Asin

---
+++ Index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const httpie = require('httpie');
+const JSDOM = require('jsdom').JSDOM;
+const Port = process.env.Port || 8888;
+
+const server = http.createServer(async (request, response) => {
+
+  const [ _, username, usePledges ] = request.url.split("/");
+
+  if (!username) return response.writeHead(500, { 'Content-Type': 'application/json' }), response.write(JSON.stringify({"error": 'username must be set.'}));
+
+  const { data } = await httpie.get('https://patreon.com/' + username);
+
+  const { document } = (new JSDOM(data)).window;
+
+  const message = (usePledges) ? document.querySelector('[data-tag="CampaignPatronEarningStats-earnings"] h2').innerHTML + "/mo" : document.querySelector('[data-tag="CampaignPatronEarningStats-patron-count"] h2').innerHTML + " patrons";
+
+  const res = {
+    schemaVersion: 1,
+    label: 'Patreon',
+    namedLogo: 'Patreon',
+    message: message,
+    color: 'FFFFFF', //FF5441
+    cacheSeconds: 60 * 60 * 8 // 8 hours
+  };
+
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(res));
+  response.end();
+  
+});
+
+server.listen(Port);
+console.log("Listening on", Port);
